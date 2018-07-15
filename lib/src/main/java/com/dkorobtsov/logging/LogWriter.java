31c5093a97d10b42d4bb845905d162ed05f4e5da
dkorobtsov <dmitri.korobtsov@gmail.com>
2018-07-15 21:16:48 +0300

    Interface clean up. Default logger implementation will be handled in Builder.

--- lib/src/main/java/com/dkorobtsov/logging/LogWriter.java
+++ lib/src/main/java/com/dkorobtsov/logging/LogWriter.java
@@ -1,19 +1,6 @@
 package com.dkorobtsov.logging;
 
-import okhttp3.internal.platform.Platform;
-
-/**
- * @author ihsan on 11/07/2017.
- */
-@SuppressWarnings({"WeakerAccess", "unused"})
 public interface LogWriter {
 
-  LogWriter DEFAULT = new LogWriter() {
-    @Override
-    public void log(int level, String tag, String message) {
-      Platform.get().log(level, message, null);
-    }
-  };
-
-  void log(int level, String tag, String msg);
+  void log(int level, String msg);
 }
