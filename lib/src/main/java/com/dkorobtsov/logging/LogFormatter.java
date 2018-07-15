b48cecc1283c5efbe58da2fb8919a4ee27c2742e
dkorobtsov <dmitri.korobtsov@gmail.com>
2018-07-15 21:15:55 +0300

    Added custom JUL formats enumerator.

---
+++ lib/src/main/java/com/dkorobtsov/logging/LogFormatter.java
@@ -0,0 +1,59 @@
+package com.dkorobtsov.logging;
+
+import java.util.Date;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+public enum LogFormatter {
+
+  JUL_DATE_MESSAGE(new SimpleFormatter() {
+    @Override
+    public synchronized String format(LogRecord lr) {
+      return String.format("[%1$tF %1$tT] %2$s %n",
+          new Date(lr.getMillis()),
+          lr.getMessage()
+      );
+    }
+  }),
+
+  JUL_DATE_LEVEL_MESSAGE(new SimpleFormatter() {
+    @Override
+    public synchronized String format(LogRecord lr) {
+      return String.format("[%1$tF %1$tT] [%2$-7s] %3$s %n",
+          new Date(lr.getMillis()),
+          lr.getLevel().getLocalizedName(),
+          lr.getMessage()
+      );
+    }
+  }),
+
+  JUL_LEVEL_MESSAGE(new SimpleFormatter() {
+    @Override
+    public synchronized String format(LogRecord lr) {
+      return String.format("%1$s %2$s %n",
+          lr.getLevel().getLocalizedName(),
+          lr.getMessage()
+      );
+    }
+  }),
+
+  JUL_MESSAGE_ONLY(new SimpleFormatter() {
+    @Override
+    public synchronized String format(LogRecord lr) {
+      return String.format("%1$s %n",
+          lr.getMessage()
+      );
+    }
+  });
+
+  SimpleFormatter formatter;
+  LogFormatter defaultFormat;
+
+  LogFormatter(SimpleFormatter simpleFormatter) {
+    this.formatter = simpleFormatter;
+  }
+
+  void setDefault(LogFormatter formatter){
+    this.defaultFormat = formatter;
+  }
+}
