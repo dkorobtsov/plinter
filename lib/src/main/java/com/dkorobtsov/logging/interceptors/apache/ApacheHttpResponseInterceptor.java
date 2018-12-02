6c0944ea8890f05aa0b2a082c02e98de51817e94
Dmitri Korobtsov <dmitri.korobtsov@gmail.com>
2018-12-03 01:35:50 +0200

    Added request url extraction from context to Apache response interceptor.

--- lib/src/main/java/com/dkorobtsov/logging/interceptors/apache/ApacheHttpResponseInterceptor.java
+++ lib/src/main/java/com/dkorobtsov/logging/interceptors/apache/ApacheHttpResponseInterceptor.java
@@ -1,18 +1,24 @@
 package com.dkorobtsov.logging.interceptors.apache;
 
 import com.dkorobtsov.logging.AbstractInterceptor;
-import com.dkorobtsov.logging.internal.ClientPrintingExecutor;
-import com.dkorobtsov.logging.internal.InterceptedResponse;
+import com.dkorobtsov.logging.Level;
 import com.dkorobtsov.logging.LoggerConfig;
 import com.dkorobtsov.logging.ResponseConverter;
-import com.dkorobtsov.logging.Level;
+import com.dkorobtsov.logging.internal.ClientPrintingExecutor;
+import com.dkorobtsov.logging.internal.InterceptedResponse;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.client.methods.HttpRequestWrapper;
 import org.apache.http.protocol.HttpContext;
 
 public class ApacheHttpResponseInterceptor extends AbstractInterceptor
     implements HttpResponseInterceptor {
 
+    private static final Logger logger = Logger
+        .getLogger(ApacheHttpResponseInterceptor.class.getName());
     private ResponseConverter<HttpResponse> responseAdapter;
 
     public ApacheHttpResponseInterceptor(LoggerConfig loggerConfig) {
@@ -25,9 +31,20 @@ public class ApacheHttpResponseInterceptor extends AbstractInterceptor
         if (loggerConfig.isLoggable && loggerConfig.level != Level.NONE) {
 
             InterceptedResponse interceptedResponse = responseAdapter.convertFrom(
-                response, null, null);
+                response, urlFrom(context), null);
 
             ClientPrintingExecutor.printResponse(loggerConfig, interceptedResponse);
+        }
+    }
+
+    URL urlFrom(HttpContext context) {
+        HttpRequestWrapper request = (HttpRequestWrapper) context.getAttribute("http.request");
+
+        try {
+            return new URL(request.getOriginal().getRequestLine().getUri());
+        } catch (MalformedURLException e) {
+            logger.log(java.util.logging.Level.SEVERE, e.getMessage(), e);
+            return null;
         }
     }
 
