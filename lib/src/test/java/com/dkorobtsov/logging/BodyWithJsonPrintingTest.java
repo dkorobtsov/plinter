7446be8ac927d78491624b61393b6165dd32052c
Dmitri Korobtsov <dmitri.korobtsov@gmail.com>
2018-12-13 14:54:40 +0200

    Split up body printing test to 3 separate classes for JSON, XML and HTML printing validation.

--- lib/src/test/java/com/dkorobtsov/logging/BodyWithJsonPrintingTest.java
+++ lib/src/test/java/com/dkorobtsov/logging/BodyWithJsonPrintingTest.java
@@ -1,20 +1,15 @@
 package com.dkorobtsov.logging;
 
 import static com.dkorobtsov.logging.internal.Util.APPLICATION_JSON;
-import static com.dkorobtsov.logging.internal.Util.APPLICATION_XML;
 import static com.dkorobtsov.logging.internal.Util.APPLICATION_ZIP;
-import static com.dkorobtsov.logging.internal.Util.TEXT_HTML;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
-import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(JUnitParamsRunner.class)
-public class InterceptorBodyPrintingTest extends BaseTest {
+public class BodyWithJsonPrintingTest extends BaseTest {
 
   private static final String SIMPLE_JSON = "{name: \"John\", age: 31, city: \"New York\"}";
 
@@ -61,34 +56,6 @@ public class InterceptorBodyPrintingTest extends BaseTest {
   private static final String MALFORMED_JSON_STARTING_WITH_INVALID_CHAR = "? \"test\" : \"test1\"}";
 
 
-  private static final String XML_BODY =
-      "<?xml version=\"1.0\" encoding=\"UTF-16\"?>"
-          + "<mammals>"
-          + "<animal id=\"0\" species=\"Capra hircus\">Goat</animal>"
-          + "<animal id=\"1\" species=\"Panthera pardus\">Leopard</animal>"
-          + "<animal id=\"2\" species=\"Equus zebra\">Zebra</animal> "
-          + "</mammals>";
-
-  private static final String MALFORMED_XML_BODY =
-      "<?xml version=\"1.0\" encoding=\"UTF-16\"?>"
-          + "<mammals>"
-          + "<animal id=\"0\" species=\"Capra hircus\">Goat</animal>"
-          + "animal id=\"1\" species=\"Panthera pardus\">Leopard</animal>"
-          + "<animal id=\"2\" species=\"Equus zebra\">Zebra</animal> "
-          + "</mammals>";
-
-  private static final String HTML_BODY = ""
-      + "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"/>"
-      + "<title>Error 404 Not Found</title></head><body>"
-      + "<div style=\"font-family:Arial,Helvetica,sans-serif;\"><h2>HTTP ERROR 404</h2>"
-      + "<pre>Not Found</pre></div><tr><th></th><th></th><th></th><th></th></tr></body></html>";
-
-  private static final String MALFORMED_HTML_BODY = ""
-      + "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"/>"
-      + "<title>Error 404 Not Found</title></head><body>"
-      + "<div style=\"font-family:Arial,Helvetica,sans-serif;\"><h2>HTTP ERROR 404</h2>"
-      + "<pre>Not Found</pre></div><tr><th></th><th><th></th><th></th></tr></body></html>";
-
   @Test
   @Parameters(method = "interceptorsWithExecutors")
   public void bodyHandling_simpleJsonRequest(String interceptor, boolean withExecutor) {
@@ -105,16 +72,6 @@ public class InterceptorBodyPrintingTest extends BaseTest {
         SIMPLE_JSON, APPLICATION_JSON, true);
 
     assertThat(loggerOutput).contains("     \"city\": \"New York\", ");
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_jsonArrayRequest(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedRequest(interceptor, withExecutor,
-        JSON_ARRAY, APPLICATION_JSON, false);
-
-    assertThat(loggerOutput).contains("{\"test1\": \"test1\"},");
-    assertThat(loggerOutput).contains("{\"test2\": \"test2\"}");
   }
 
   @Test
@@ -163,6 +120,16 @@ public class InterceptorBodyPrintingTest extends BaseTest {
 
   @Test
   @Parameters(method = "interceptorsWithExecutors")
+  public void bodyHandling_jsonArrayRequest(String interceptor, boolean withExecutor) {
+    final List<String> loggerOutput = interceptedRequest(interceptor, withExecutor,
+        JSON_ARRAY, APPLICATION_JSON, false);
+
+    assertThat(loggerOutput).contains("{\"test1\": \"test1\"},");
+    assertThat(loggerOutput).contains("{\"test2\": \"test2\"}");
+  }
+
+  @Test
+  @Parameters(method = "interceptorsWithExecutors")
   public void bodyHandling_JsonRequestWithInvalidChar(String interceptor, boolean withExecutor) {
     final List<String> loggerOutput = interceptedRequest(interceptor, withExecutor,
         MALFORMED_JSON_STARTING_WITH_INVALID_CHAR, APPLICATION_JSON, false);
@@ -199,108 +166,6 @@ public class InterceptorBodyPrintingTest extends BaseTest {
 
   @Test
   @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_htmlRequest(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedRequest(interceptor, withExecutor,
-        HTML_BODY, TEXT_HTML, false);
-
-    assertThat(loggerOutput).contains("<title>Error 404 Not Found</title>");
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_htmlResponse(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedResponse(interceptor, withExecutor,
-        HTML_BODY, "text/html", false);
-
-    assertThat(loggerOutput).contains("<title>Error 404 Not Found</title>");
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_malformedHtmlRequest(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedRequest(interceptor, withExecutor,
-        MALFORMED_HTML_BODY, TEXT_HTML, false);
-
-    loggerOutput
-        .stream()
-        .filter(it ->
-            it.startsWith(
-                "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"/>"))
-        .findFirst()
-        .orElseThrow(() ->
-            new AssertionError("Interceptor should be able to handle html request body."));
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_malformedHtmlResponse(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedResponse(interceptor, withExecutor,
-        MALFORMED_HTML_BODY, "text/html", false);
-
-    loggerOutput
-        .stream()
-        .filter(it ->
-            it.startsWith(
-                "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"/>"))
-        .findFirst()
-        .orElseThrow(() ->
-            new AssertionError("Interceptor should be able to handle html response body."));
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_xmlRequest(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedRequest(interceptor, withExecutor,
-        XML_BODY, APPLICATION_XML, false);
-
-    assertThat(loggerOutput).contains("<?xml version=\"1.0\" encoding=\"UTF-16\"?>");
-    assertThat(loggerOutput).contains("</mammals>");
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_xmlResponse(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedResponse(interceptor, withExecutor,
-        XML_BODY, APPLICATION_XML, false);
-
-    assertThat(loggerOutput).contains("<?xml version=\"1.0\" encoding=\"UTF-16\"?>");
-    assertThat(loggerOutput).contains("</mammals>");
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_malformedXmlRequest(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedRequest(interceptor, withExecutor,
-        MALFORMED_XML_BODY, APPLICATION_XML, false);
-
-    loggerOutput
-        .stream()
-        .filter(it ->
-            it.startsWith(
-                "<?xml version=\"1.0\" encoding=\"UTF-16\"?><mammals>"))
-        .findFirst()
-        .orElseThrow(() ->
-            new AssertionError("Interceptor should be able to handle xml request body."));
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
-  public void bodyHandling_malformedXmlResponse(String interceptor, boolean withExecutor) {
-    final List<String> loggerOutput = interceptedResponse(interceptor, withExecutor,
-        MALFORMED_XML_BODY, APPLICATION_XML, false);
-
-    loggerOutput
-        .stream()
-        .filter(it ->
-            it.startsWith(
-                "<?xml version=\"1.0\" encoding=\"UTF-16\"?><mammals>"))
-        .findFirst()
-        .orElseThrow(() ->
-            new AssertionError("Interceptor should be able to handle xml response body."));
-  }
-
-  @Test
-  @Parameters(method = "interceptorsWithExecutors")
   public void bodyHandling_fileRequest(String interceptor, boolean withExecutor) {
     final List<String> loggerOutput = interceptedRequest(interceptor, withExecutor,
         PREFORMATTED_JSON_BODY, APPLICATION_ZIP, true);
@@ -312,10 +177,10 @@ public class InterceptorBodyPrintingTest extends BaseTest {
   @Parameters(method = "interceptorsWithExecutors")
   public void bodyHandling_fileResponse(String interceptor, boolean withExecutor) {
     final List<String> loggerOutput = interceptedResponse(interceptor, withExecutor,
-        PREFORMATTED_JSON_BODY, APPLICATION_ZIP,
-        true);
+        PREFORMATTED_JSON_BODY, APPLICATION_ZIP, true);
 
     assertThat(loggerOutput).contains("  Omitted response body ");
   }
 
 }
+
