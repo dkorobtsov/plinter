bbf5e0fe3d4b85244cc7e522d13bb8b123238a1c
Dmitri Korobtsov <dmitri.korobtsov@gmail.com>
2023-09-14 00:03:33 +0300

    Make lib Java 8 compatible.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,6 @@ plugins {
   `maven-publish`
 }
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
-}
-
 configure<NexusPublishExtension> {
   repositories.sonatype {
     nexusUrl.set(
@@ -79,8 +74,8 @@ allprojects {
   }
 
   java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
   }
 
   tasks.withType(JavaCompile::class) {
