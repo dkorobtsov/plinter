3f65ea1070c8eb3eb458159955eeacab5b8077f5
Dmitri Korobtsov <dmitri.korobtsov@gmail.com>
2025-04-15 16:43:11 +0300

    Fix PMD warning

--- interceptor-tests/src/test/java/io/github/dkorobtsov/tests/BaseTest.java
+++ interceptor-tests/src/test/java/io/github/dkorobtsov/tests/BaseTest.java
@@ -48,7 +48,6 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
